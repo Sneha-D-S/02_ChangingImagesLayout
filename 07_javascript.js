08c7a57743f9624b3c2841de2474707e7a4e9d7e
Sneha-D-S <64400673+Sneha-D-S@users.noreply.github.com>
2022-07-29 20:17:18 +0530

    Add files via upload

---
+++ 07_javascript.js
@@ -0,0 +1,9 @@
+function myEvt1() {
+  document.getElementById("c1").id = "c2";
+}
+function myEvt2() {
+  document.getElementById("c3").id = "c2";
+}
+function myEvt3() {
+  document.getElementById("c3").innerHTML = "An image comes here";
+}
